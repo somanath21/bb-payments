cf13cc80387beff4ea64635db640dd3d7bb57114
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-05 19:42:10 +0530

    test-compliance

---
+++ examples/bb-payment/test_entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
+helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
+# helm test g2p-sandbox -n paymenthub
