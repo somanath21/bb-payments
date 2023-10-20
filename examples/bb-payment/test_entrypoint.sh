bc09adccfa7aa8da6a09adf3f81e7d2c07800c2f
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-20 15:54:53 +0530

    helm upgrade through test_entrypoint 8

--- examples/bb-payment/test_entrypoint.sh
+++ examples/bb-payment/test_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install helm
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
