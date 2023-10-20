bbfb431859db727c2ae9f8af9e107d7e409c0fc0
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-20 16:01:07 +0530

    test harness with helm install

--- examples/bb-payment/test_entrypoint.sh
+++ examples/bb-payment/test_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
