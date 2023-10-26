ed86bd9842866bfb48b3a6329ced259d90e29a3c
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-26 17:01:43 +0530

    test eks context

--- examples/bb-payment/test_entrypoint.sh
+++ examples/bb-payment/test_entrypoint.sh
@@ -3,11 +3,16 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-# role_arn: "arn:aws:iam::161826879607:role/EXT-mifosGroupIAMRole"
-# region: AWS_REGION
-# profile_name: "playground"
-# role_session_name: deploy-test
-# session_duration: '1800'
+# Access environment variables from the context
+AWS_ROLE_ARN="${AWS_CIRCLECI_ROLE_ARN}"
+AWS_REGION="${AWS_REGION}"
+
+# Set AWS CLI configuration
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set aws_session_token "$AWS_SESSION_TOKEN"
+aws configure set default.region "$AWS_REGION"
+
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
