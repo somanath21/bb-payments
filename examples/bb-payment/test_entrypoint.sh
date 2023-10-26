6d0e90ebf9e862bb67817bbde33a4b37ca6e2485
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-26 19:32:01 +0530

    test eks context

--- examples/bb-payment/test_entrypoint.sh
+++ examples/bb-payment/test_entrypoint.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-# curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# Access environment variables from the context
-AWS_ROLE_ARN="${AWS_CIRCLECI_ROLE_ARN}"
-AWS_REGION="${AWS_REGION}"
 
-# Set AWS CLI configuration
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set aws_session_token "$AWS_SESSION_TOKEN"
-aws configure set default.region "$AWS_REGION"
+# # Access environment variables from the CircleCI context
+# AWS_ROLE_ARN="${AWS_CIRCLECI_ROLE_ARN}"
+# AWS_REGION="${AWS_REGION}"
+
+# # Set AWS CLI configuration
+# aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+# aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+# aws configure set aws_session_token "$AWS_SESSION_TOKEN"
+# aws configure set default.region "$AWS_REGION"
+
+# Set your AWS credentials and AWS CLI configuration
+AWS_CIRCLECI_ROLE_ARN="your-role-arn"
+AWS_REGION="your-aws-region"
+EKS_CLUSTER="your-eks-cluster"
+
+# Assume the specified AWS IAM role
+aws sts assume-role --role-arn "$AWS_CIRCLECI_ROLE_ARN" --role-session-name deploy-test --duration-seconds 1800 > assumed-roles.json
+
+# Extract the temporary credentials from the assumed role response
+export AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId assumed-roles.json)
+export AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey assumed-roles.json)
+export AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken assumed-roles.json)
+
+# Update the kubeconfig for the specified EKS cluster
+aws eks update-kubeconfig --name "$EKS_CLUSTER" --region "$AWS_REGION" --profile playground
+
+# Clean up temporary role assumption file
+rm assumed-roles.json
 
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
