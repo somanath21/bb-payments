2d9df80178ec4e7c8613ace8d4b3a7c9bedcc023
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-27 19:03:38 +0530

    test eks context

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -19,7 +19,7 @@ export AWS_REGION=$AWS_REGION
 AWS_ROLE_ARN=$AWS_CIRCLECI_ROLE_ARN
 AWS_REGION=$AWS_REGION
 
-aws eks update-kubeconfig --name GStack-sb-eks-plg
+# aws eks update-kubeconfig --name GStack-sb-eks-plg
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
