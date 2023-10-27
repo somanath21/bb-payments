f36366aee1862770e29d92bd0d1672d640f938f0
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-27 18:21:52 +0530

    test eks context

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -16,22 +16,9 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 # AWS_REGION="your-aws-region"
 # EKS_CLUSTER="your-eks-cluster"
 
-# # Assume the specified AWS IAM role
-# aws sts assume-role --role-arn "$AWS_CIRCLECI_ROLE_ARN" --role-session-name deploy-test --duration-seconds 1800 > assumed-roles.json
-
-# # Extract the temporary credentials from the assumed role response
-# export AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId assumed-roles.json)
-# export AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey assumed-roles.json)
-# export AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken assumed-roles.json)
-
-# # Update the kubeconfig for the specified EKS cluster
-# aws eks update-kubeconfig --name "$EKS_CLUSTER" --region "$AWS_REGION" --profile playground
-
-# # Clean up temporary role assumption file
-# rm assumed-roles.json
 # Access environment variables from CircleCI context directly
-AWS_ROLE_ARN=arn:aws:iam::161826879607:role/EXT-mifosGroupIAMRole
-AWS_REGION=eu-central-1
+AWS_ROLE_ARN=$AWS_CIRCLECI_ROLE_ARN
+AWS_REGION=$AWS_REGION
 aws eks update-kubeconfig --name GStack-sb-eks-plg
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
