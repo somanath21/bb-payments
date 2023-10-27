bc7f2a3d8a4006d870e5ec8013f594206741232c
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-27 16:50:04 +0530

    test eks context

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-# Access environment variables from the CircleCI context
-AWS_ROLE_ARN="${AWS_CIRCLECI_ROLE_ARN}"
-AWS_REGION="${AWS_REGION}"
+# # Access environment variables from the CircleCI context
+# AWS_ROLE_ARN="${AWS_CIRCLECI_ROLE_ARN}"
+# AWS_REGION="${AWS_REGION}"
 
-# Set AWS CLI configuration
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set aws_session_token "$AWS_SESSION_TOKEN"
-aws configure set default.region "$AWS_REGION"
+# # Set AWS CLI configuration
+# aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+# aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+# aws configure set aws_session_token "$AWS_SESSION_TOKEN"
+# aws configure set default.region "$AWS_REGION"
 
 # # Set your AWS credentials and AWS CLI configuration
 # AWS_CIRCLECI_ROLE_ARN="your-role-arn"
@@ -30,6 +30,7 @@ aws configure set default.region "$AWS_REGION"
 # # Clean up temporary role assumption file
 # rm assumed-roles.json
 
+aws eks update-kubeconfig --name GStack-sb-eks-plg
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
