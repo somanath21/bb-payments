6930921d78f51f6399b8d005b90ea8fc63af6340
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-30 13:47:11 +0530

    test with profile aws configure with set values

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -15,8 +15,9 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 # echo "AWS_ROLE_ARN: $AWS_ROLE_ARN"
 # echo "AWS_REGION: $AWS_REGION"
 
-aws configure --profile playground
+
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
+aws configure --profile playground
 aws eks update-kubeconfig --name GStack-sb-eks-plg
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
