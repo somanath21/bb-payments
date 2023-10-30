cc7810855d6cf523f933b54d21c8753bb0bbefeb
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-30 17:57:57 +0530

    test with eks orbs code

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-echo "setting cluster"
-CLUSTER_NAME=$(eval echo "$EKS_CLUSTER")
-AWS_REGION=$(eval echo "$AWS_REGION")
+CLUSTER_NAME="$EKS_CLUSTER"
+AWS_REGION="$AWS_REGION"
 AWS_PROFILE=playground
-ROLE_ARN=$(eval echo "$AWS_CIRCLECI_ROLE_ARN")
-echo "set the cluster"
+ROLE_ARN="$AWS_CIRCLECI_ROLE_ARN"
 
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
