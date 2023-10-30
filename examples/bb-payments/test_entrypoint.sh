4a6dc5372d01ebc4d7460fe409e72039ff8c7ebc
Somanath Hugar <somanathhugar@fynarfin.io>
2023-10-30 13:58:13 +0530

    test with profile aws configure with set values

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -15,10 +15,52 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 # echo "AWS_ROLE_ARN: $AWS_ROLE_ARN"
 # echo "AWS_REGION: $AWS_REGION"
 
+ROLE_SESSION_NAME= deploy-test
+AWS_CIRCLECI_ROLE_ARN=$(circleci env subst "${AWS_CIRCLECI_ROLE_ARN}")
+PROFILE_NAME= playground
+SESSION_DURATION= 1800
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
-aws configure --profile playground
-aws eks update-kubeconfig --name GStack-sb-eks-plg
+if [ -z "${ROLE_SESSION_NAME}" ]; then
+    echo "Role session name is required"
+    exit 1
+fi
+
+if [ -z "${CIRCLE_OIDC_TOKEN_V2}" ]; then
+    echo "OIDC Token cannot be found. A CircleCI context must be specified."
+    exit 1
+fi
+
+if [ ! "$(command -v aws)" ]; then
+    echo "AWS CLI is not installed. Please run the setup or install command first."
+    exit 1
+fi
+
+read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<EOF
+$(aws sts assume-role-with-web-identity \
+--role-arn "${AWS_CIRCLECI_ROLE_ARN}" \
+--role-session-name "${ROLE_SESSION_NAME}" \
+--web-identity-token "${CIRCLE_OIDC_TOKEN_V2}" \
+--duration-seconds "${SESSION_DURATION}" \
+--query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+--output text)
+EOF
+
+if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_SESSION_TOKEN}" ]; then
+    echo "Failed to assume role";
+    exit 1
+else 
+    {
+        echo "export AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
+        echo "export AWS_SECRET_ACCESS_KEY=\"${AWS_SECRET_ACCESS_KEY}\""
+        echo "export AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
+    } >>"$BASH_ENV"
+    echo "Assume role with web identity succeeded"
+fi
+
+
+# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
+# aws configure --profile playground
+# aws eks update-kubeconfig --name GStack-sb-eks-plg
 helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
