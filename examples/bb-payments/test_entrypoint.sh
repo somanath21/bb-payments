f44ffc34f47aa087165817c5f264ee30207c57aa
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-12 20:07:12 +0530

    install helm

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -13,6 +13,9 @@ cat  /home/circleci/parameters.json
 #     session_duration: '1800'  
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
 # aws eks update-kubeconfig --name sandbox
