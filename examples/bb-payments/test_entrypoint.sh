f23abcbe8183206c376e4bde0bb999157858c2e6
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-12 20:05:08 +0530

    install helm

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -13,7 +13,8 @@ cat  /home/circleci/parameters.json
 #     session_duration: '1800'  
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
-
+sudo apt-get update
+sudo apt-get install helm
 # aws eks update-kubeconfig --name sandbox
 helm test g2p-sandbox -n paymenthub
 # helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
