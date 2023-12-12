b945e8ae85ae1fef555db0ab5107d17f179b3324
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-12 19:34:19 +0530

    test with cluster credentials

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
-
-# CLUSTER_NAME="$EKS_CLUSTER"
-# AWS_REGION="$AWS_REGION"
-# AWS_PROFILE=playground
-# ROLE_ARN="$role_arn"
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_REGION
+echo $AWS_CIRCLECI_ROLE_ARN
+echo $EKS_CLUSTER
+echo $SANDBOX_DOMAIN
+cat  /home/circleci/parameters.json
+chmod u+x entrypoint.sh
 
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
-# aws eks update-kubeconfig --name GStack-sb-eks-plg
-helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
-helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
+
+aws eks update-kubeconfig --name sandbox
+helm test g2p-sandbox -n paymenthub
+# helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
+# helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
