28ee91068a0f2ca31b84095025d73994186b1418
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-12 19:58:15 +0530

    test with cluster credentials

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -6,11 +6,15 @@ echo $EKS_CLUSTER
 echo $SANDBOX_DOMAIN
 cat  /home/circleci/parameters.json
 # chmod u+x entrypoint.sh
-
+# - aws-cli/setup:
+#     role_arn: "${AWS_CIRCLECI_ROLE_ARN}"
+#     region: ${AWS_REGION}
+#     role_session_name: CircleCISession
+#     session_duration: '1800'  
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
 
-aws eks update-kubeconfig --name sandbox
+# aws eks update-kubeconfig --name sandbox
 helm test g2p-sandbox -n paymenthub
 # helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 # helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
