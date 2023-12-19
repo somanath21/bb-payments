4e747f81484d9b2c9f0c76c98a93f8d48478e266
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-19 15:23:28 +0530

    helm deploy and helm test with wait time

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 #insatll kubectl
-# apt-get update
-# # apt-transport-https may be a dummy package; if so, you can skip that package
-# apt-get install -y apt-transport-https ca-certificates curl gpg
-
-# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |  gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-# # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' |  tee /etc/apt/sources.list.d/kubernetes.list
-
-# apt-get update
-# apt-get install -y kubelet kubeadm kubectl
-# apt-mark hold kubelet kubeadm kubectl
-# kubectl version
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p /etc/apt/keyrings
@@ -29,8 +16,6 @@ sudo dpkg -i minikube_latest_amd64.deb
 minikube start
 
 minikube kubectl -- get po -A #Interact with Minikube cluster
-kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
-echo "service-monitor installed"
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region eu-central-1
 # aws configure --profile playground
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
@@ -40,7 +25,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.
 sudo apt-get update
 sudo apt-get install helm
 #payment install
-# kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml #Install ServiceMonitor
+kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml #Install ServiceMonitor
 kubectl create namespace paymenthub
 kubectl get -A namespace  
 helm repo add g2p-sandbox-1-3-1 https://fynarfin.io/images/ph-ee-g2psandbox-1.3.1/
@@ -48,7 +33,7 @@ helm install my-ph-ee-g2psandbox g2p-sandbox-1-3-1/ph-ee-g2psandbox --version 1.
 
 kubectl get pods -n paymenthub
 helm list -n paymenthub
-helm test my-ph-ee-g2psandbox --wait --timeout 300
+helm test my-ph-ee-g2psandbox --timeout 300
 # helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 # helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
