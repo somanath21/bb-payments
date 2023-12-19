34e9aa4da04e3f5bee7ce301c460da3adebeab9a
Somanath Hugar <somanathhugar@fynarfin.io>
2023-12-19 15:29:06 +0530

    helm deploy and helm test with wait time

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -33,7 +33,7 @@ helm install my-ph-ee-g2psandbox g2p-sandbox-1-3-1/ph-ee-g2psandbox --version 1.
 
 kubectl get pods -n paymenthub
 helm list -n paymenthub
-helm test my-ph-ee-g2psandbox --timeout 10m
+helm test my-ph-ee-g2psandbox -n paymenthub --timeout 10m
 # helm upgrade --install -f helm/govstack-chart/values.yaml g2p-sandbox helm/govstack-chart --create-namespace  --namespace paymenthub
 # helm upgrade -f helm/fineract/values.yaml fineract helm/fineract --install --create-namespace --namespace fineract
 # helm test g2p-sandbox -n paymenthub
