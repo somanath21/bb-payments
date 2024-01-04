dcdcf57dc651131118e780b6d3691f1b6946bcc2
Somanath Hugar <somanathhugar@fynarfin.io>
2024-01-04 14:06:10 +0530

    wait for the pods and git clone for kibana

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -30,12 +30,13 @@ kubectl get -A namespace
 helm repo add g2p-sandbox-1-3-1 https://fynarfin.io/images/ph-ee-g2psandbox-1.3.1/
 helm install ph-ee-g2psandbox g2p-sandbox-1-3-1/ph-ee-g2psandbox --version 1.3.1 -n paymenthub 
 sleep 300
+kubectl get pods -n paymenthub
 
 git clone -b 7.17 https://github.com/elastic/helm-charts.git elastic/helm-charts
 # sleep 300
 cd elastic/helm-charts/elasticsearch/examples/security/
 make secrets || echo "elastic-secrets" already exists
-
+git clone -b 7.17 https://github.com/elastic/helm-charts.git elastic/helm-charts
 cd elastic/helm-charts/kibana/examples/security/
 make secrets || echo "kibana-secrets" already exists
 #insatll netcat
