43c4d644241353c80c1ee142d37407f43af13179
Somanath Hugar <somanathhugar@fynarfin.io>
2024-01-11 16:06:14 +0530

    install kubectl and helm

--- examples/bb-payments/test_entrypoint.sh
+++ examples/bb-payments/test_entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p /etc/apt/keyrings
+sudo touch -y /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+#helm install
+sudo apt-get update
+sudo apt-get install helm
+
 kubectl get pods -n paymenthub
 helm test ph-ee-g2psandbox -n paymenthub --timeout 5m
 # no_output_timeout=900
